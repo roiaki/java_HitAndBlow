83b140d2e5f1a0201b849a2be7732e96ae0bae80
hagi <hahagigi77@gmail.com>
2020-06-26 18:58:21 +0900

    first push

---
+++ Free/src/paiza_d/D156.java
@@ -0,0 +1,16 @@
+package paiza_d;
+
+import java.util.Scanner;
+
+public class D156 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int area = sc.nextInt();
+		int number = sc.nextInt();
+
+		int ans = area * number;
+
+		System.out.println(ans);
+	}
+}
