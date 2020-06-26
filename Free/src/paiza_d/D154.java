8251386831e1a374a7e3e79d7a45b62b0aa4387f
hagi <hahagigi77@gmail.com>
2020-06-27 01:23:07 +0900

    d154

---
+++ Free/src/paiza_d/D154.java
@@ -0,0 +1,18 @@
+package paiza_d;
+
+import java.util.Scanner;
+
+public class D154 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int divisionNum = sc.nextInt();
+		int personNum = sc.nextInt();
+
+		int totalDivision = divisionNum * divisionNum;
+
+		int ans = totalDivision - personNum;
+
+		System.out.println(ans);
+	}
+}
