a0f9a627fba24a8f28b21d516f466c6d782b4c42
hagi <hahagigi77@gmail.com>
2020-06-27 01:24:56 +0900

    02

--- Free/src/paiza_d/D154.java
+++ Free/src/paiza_d/D154.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class D154 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
 		int divisionNum = sc.nextInt();
 		int personNum = sc.nextInt();
 
